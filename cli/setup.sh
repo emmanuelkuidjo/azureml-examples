aab99f80dd13d90cf6f717f07225f3dd0a01f830
Emmanuel KUIDJO <emmanuelkuidjo@Emmanuels-Mac.local>
2024-07-06 13:24:13 +0200

    update workflow

--- cli/setup.sh
+++ cli/setup.sh
@@ -25,9 +25,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
-LOCATION="eastus"
-WORKSPACE="main"
+GROUP="rg-azml"
+LOCATION="swedencentral"
+WORKSPACE="thehaighway1"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
